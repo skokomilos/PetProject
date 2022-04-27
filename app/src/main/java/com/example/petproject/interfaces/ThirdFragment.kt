ca47bb303005845b48b26d01d6f2d77aacaf69e8
skokomilos <berkaskoko@gmail.com>
2022-04-27 13:39:23 +0200

    add not not important interface

--- app/src/main/java/com/example/petproject/interfaces/ThirdFragment.kt
+++ app/src/main/java/com/example/petproject/interfaces/ThirdFragment.kt
@@ -1,8 +1,13 @@
-package com.example
+package com.example.petproject.interfaces
+
+import androidx.fragment.app.Fragment
 
 /**
  *
  * @author berka on 4/27/22
  */
-class ThirdFragment {
+class ThirdFragment : Fragment() {
+
+
+
 }
