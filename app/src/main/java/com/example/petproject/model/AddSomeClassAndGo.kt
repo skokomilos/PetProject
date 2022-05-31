bb7b28c648871fc45747de7defa3b7c18d240c5a
skokomilos <berkaskoko@gmail.com>
2022-05-31 11:21:14 +0200

    add comment 3

--- app/src/main/java/com/example/petproject/model/AddSomeClassAndGo.kt
+++ app/src/main/java/com/example/petproject/model/AddSomeClassAndGo.kt
@@ -7,5 +7,7 @@ package com.example.petproject.model
 class AddSomeClassAndGo(val name: String, val age: Int) {
     //add comment 1
     //add comment 2
+    //add comment 3
+    //add comment 4
 
 }
