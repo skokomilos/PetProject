dda8a7a0c326d65b49a9d4c9a0a129b2cd6fd127
skokomilos <berkaskoko@gmail.com>
2022-07-08 12:40:37 +0200

    some changes one

--- app/src/main/java/com/example/petproject/model/AddSomeClassAndGo.kt
+++ app/src/main/java/com/example/petproject/model/AddSomeClassAndGo.kt
@@ -1,5 +1,4 @@
 package com.example.petproject.model
-
 /**
  *
  * @author berka on 5/31/22
@@ -9,5 +8,7 @@ class AddSomeClassAndGo(val name: String, val age: Int) {
     //add comment 2
     //add comment 3
     //add comment 4
-
+    val some_number = 4
+    private fun hero(one: String, two: String){}
 }
+
