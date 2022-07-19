8f3c9db7640ab446cf13011b786f20268572bff7
skokomilos <berkaskoko@gmail.com>
2022-07-19 16:45:25 +0200

    changes here

---
+++ app/src/main/java/com/example/petproject/model/VeryNewClass19.kt
@@ -0,0 +1,20 @@
+package com.example.petproject.model
+
+/**
+ *
+ * @author berka on 7/19/22
+ */
+class VeryNewClass19 {
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+    //here
+}
